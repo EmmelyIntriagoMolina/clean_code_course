e95df76c585c7f168c3f3787f35e6d6d4a2f2020
Emmely Gabriela <gabyintriago07@gmail.com>
2025-07-14 21:32:33 -0500

    Funciones cortas

--- src/clean_code/03-functions.ts
+++ src/clean_code/03-functions.ts
@@ -40,4 +40,12 @@
         return true;        
 
     }
+
+    // Continuar
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        if ( isDead )  return 1500;
+        if ( isSeparated ) return 2500;
+        if ( isRetired ) return 3000 
+        return 4000
+    }
 })();
